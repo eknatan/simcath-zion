33ab92fdb70bb590dc084b340372f36e4f2590d0
natan ek <ek.natan@gmail.com>
2025-10-21 22:47:21 +0300

    update

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,11 +2,11 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 // Can be imported from a shared config
-const locales = ['he', 'en'];
+const locales = ['he', 'en'] as const;
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locales.includes(locale as typeof locales[number])) notFound();
 
   return {
     messages: (await import(`../messages/${locale}.json`)).default
